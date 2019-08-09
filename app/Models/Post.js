1eb578d3e81571957135838115bf2b16f784d575
gario300 <41522264+gario300@users.noreply.github.com>
2019-08-09 03:38:25 -0500

    a

--- app/Models/Post.js
+++ app/Models/Post.js
@@ -8,7 +8,7 @@ class Post extends Model {
         return this.belongsTo('App/Models/User')
     }
     replies() {
-        return this.hasMany('App/Models/Reply', 'post_id')
+        return this.hasMany('App/Models/Reply', 'post_id'.pivotTable('posts'))
     }
     favorites() {
         return this.hasMany('App/Models/Favorite')
