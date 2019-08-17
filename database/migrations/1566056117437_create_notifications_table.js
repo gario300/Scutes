5fa5c132f132f9829859901318564d124572d52a
gario300 <41522264+gario300@users.noreply.github.com>
2019-08-17 11:27:55 -0500

    Update 1566056117437_create_notifications_table.js

--- database/migrations/1566056117437_create_notifications_table.js
+++ database/migrations/1566056117437_create_notifications_table.js
@@ -5,18 +5,19 @@ const Schema = use('Schema')
 class NotificationsTableSchema extends Schema {
 
   up () {
-    this.create('notifications', (table) => {
-      table.increments()
-      table.string('type').nullable()
+    this.createIfNotExists('notifications', table => {
+      table.uuid('id').primary()
+      table.string('type')
       table.integer('notifiable_id').unsigned().nullable()
       table.string('notifiable_type').nullable()
-      table.jsonb('data').nullable()
+      table.jsonb('data')
+      table.timestamp('read_at').nullable()
       table.timestamps()
     })
   }
 
   down () {
-    this.drop('notifications')
+    this.dropIfExists('notifications')
   }
 
 }
