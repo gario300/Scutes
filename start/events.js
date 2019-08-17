3dfe7d4234a14b47714ea2ffb6e0425015e0043b
gario300 <41522264+gario300@users.noreply.github.com>
2019-08-17 15:59:25 -0500

    Update events.js

--- start/events.js
+++ start/events.js
@@ -1,6 +1,12 @@
 const Event = use('Event')
+const Notification = use('App/Models/Notification')
 
 Event.on('new::reply',async (reply) => {
-  await console.log('holamundo')
+  const notiData = request.only(['user_id','sender_id','reply_id']);
+  const noti = new Notification();
+        noti.user_id = user_id;
+        noti.sender_id = sender_id;
+        noti.reply_id = reply_id;    
+        await noti.save();
     
   })
