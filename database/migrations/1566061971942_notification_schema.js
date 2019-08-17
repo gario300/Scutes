3fc724d0e67276f13cb5b22e818db5f419a7abdb
gario300 <41522264+gario300@users.noreply.github.com>
2019-08-17 14:01:21 -0500

    Update 1566061971942_notification_schema.js

--- database/migrations/1566061971942_notification_schema.js
+++ database/migrations/1566061971942_notification_schema.js
@@ -7,7 +7,7 @@ class NotificationSchema extends Schema {
   up () {
     this.create('notifications', (table) => {
       table.increments()
-      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.integer('user_id').nullable()
       table.boolean('is_readed').defaultTo(false)
     table.timestamps()
     })
