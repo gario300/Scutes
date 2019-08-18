3e4f450d454fb017f739b57c2072b011ded3d4d5
gario300 <41522264+gario300@users.noreply.github.com>
2019-08-18 13:04:23 -0500

    Update NotificationController.js

--- app/Controllers/Http/NotificationController.js
+++ app/Controllers/Http/NotificationController.js
@@ -26,7 +26,9 @@ class NotificationController {
         
         const user = auth.current.user
         try {
-            const notifications = await Notification.findByOrFail('receptor_id', user.id)
+            const notifications = await Notification.findByOrFail('receptor_id', user.id )
+                .with('user')
+                .with('post')
                 .orderBy('created_at', 'DESC')
                 .fetch()
     
