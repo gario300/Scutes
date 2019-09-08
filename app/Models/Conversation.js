d68492081694bf199ce5af9e94121bbed00201f2
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-08 01:30:16 -0500

    Update Conversation.js

--- app/Models/Conversation.js
+++ app/Models/Conversation.js
@@ -5,7 +5,8 @@ const Model = use('Model')
 
 class Conversation extends Model {
     users(){
-      return this.belongsToMany('App/Models/User')
+      return this.belongsToMany('App/Models/User', 'userone', 'usertwo')
+      .pivotTable('conversations')
       } 
 }
 
