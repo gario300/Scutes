1a400d864f3d6d250a8e7a56e008904558fb6c52
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-08 01:09:44 -0500

    Update ConversationController.js

---
+++ app/Controllers/Http/ConversationController.js
@@ -0,0 +1,47 @@
+'use strict'
+const User = use('App/Models/User')
+const Conversation = use('App/Models/Conversation')
+const Sender = use('App/Models/Seender')
+
+class ConversationController {
+    
+    async newconversation({auth, request, response}){
+        const seender = auth.current.user
+        const data = request.only(['receptor_id']);
+        
+        const conversation = await Conversation.findOrCreate(
+            { usertwo: data.receptor_id },
+            { userone: seender.id, usertwo: data.receptor_id }
+          )
+          
+          return response.json({
+            status: 'success',
+            data: conversation
+        })
+        
+    } 
+
+    async getconversation({auth, response}){
+
+        const user = auth.current.user
+        
+        const conversations1  = await Conversation.query()  
+        .where('userone', user.id)
+        .with('users')
+        .fetch()
+        
+        const conversations2 = await Conversation.query()
+        .where('usertwo', user.id)
+        .with('users')
+        .fetch()
+
+        return response.json({
+            status: 'success',
+            data: conversations1, conversations2
+      })
+    }
+
+    
+}
+
+module.exports = ConversationController
