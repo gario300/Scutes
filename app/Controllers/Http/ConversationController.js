d0a44076381fcdc8929d6b5359e3f1505b360618
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-08 19:52:26 -0500

    Update ConversationController.js

--- app/Controllers/Http/ConversationController.js
+++ app/Controllers/Http/ConversationController.js
@@ -53,11 +53,6 @@ class ConversationController {
     async getconversationbyid({params, response}){
       
       const conversation  = await Conversation.query()
-      .select('user1.username AS Emisor', 
-        'user2.username AS Receptor',
-        )
-        .join('users as user1', 'conversations.from_user_id', '=', 'user1.id')
-        .join('users as user2', 'conversations.to_user_id', '=', 'user2.id')
         .where('id', params.id)
         .with('seenders')
         .fetch()
