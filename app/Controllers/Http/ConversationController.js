d7c3a76796516bf8c2b062a0db3c779c39867b3e
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-08 19:02:44 -0500

    Update ConversationController.js

--- app/Controllers/Http/ConversationController.js
+++ app/Controllers/Http/ConversationController.js
@@ -34,14 +34,13 @@ class ConversationController {
         'user1.avatar AS Emisoravatar',
         'user2.avatar AS Receptoravatar'
         )
-        .select('id')
-        .from('conversations')
         .join('users as user1', 'conversations.from_user_id', '=', 'user1.id')
-        .join('users as user2', 'conversations.to_user_id', '=', 'user2.id') 
+        .join('users as user2', 'conversations.to_user_id', '=', 'user2.id')
         .where(function () {
           this.orWhere('from_user_id', me.id)
           this.orWhere('to_user_id', me.id )
         })
+        .with('id')
         .with('seenders')
         .fetch()
         
