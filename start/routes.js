2df36c04de8afa24cd456161a7fea7fe992bb6f4
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-15 13:55:15 -0500

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -60,11 +60,11 @@ const Route = use('Route')
     .prefix('mensajeria')
     .middleware(['auth']);
 
-    Route.get('/timeline/:page', 'UserController.timeline')
     Route.group(() => {
       Route.get('/users_to_follow', 'UserController.usersToFollow');
       Route.post ( '/follow' , 'UserController.follow' );
       Route.delete('/unfollow/:id', 'UserController.unFollow');
+      Route.get('/timeline/:page', 'UserController.timeline')
   })
       .prefix('users')
       .middleware(['auth']);
