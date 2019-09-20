3ea71ea889d845fd11cfbd35a16e8a4f3be3d20a
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-20 01:05:59 -0500

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -75,7 +75,7 @@ const Route = use('Route')
       Route.post ( '/follow' , 'UserController.follow' );
       Route.delete('/unfollow/:id', 'UserController.unFollow');
       Route.get('/timeline/:page', 'UserController.timeline')
-      Route.get('/postusers/:id?foo=', 'PostController.showprofileposts')
+      Route.get('/postusers/:id', 'PostController.showprofileposts')
   })
       .prefix('users')
       .middleware(['auth']);
