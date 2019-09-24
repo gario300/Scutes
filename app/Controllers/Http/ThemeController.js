49ad7f2940ad2805a1a0dfe094e00f4a85933ffd
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-24 01:03:39 -0500

    Update ThemeController.js

--- app/Controllers/Http/ThemeController.js
+++ app/Controllers/Http/ThemeController.js
@@ -204,7 +204,7 @@ class ThemeController {
                     .leftJoin('themes as theme', 'IT.theme_id', '=', 'theme.id')
                     .whereNot('theme_id', null)
                     .whereNot('user_id', null)
-                    .where('creador', 'creador')
+                    .where('creador', store.creador)
                     .orderBy('created', 'DESC')
                     .paginate(params.page, 3)
                     
