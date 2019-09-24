fb834ed0cb0473b24440b2ab43b599a5e9c2e1cc
gario300 <41522264+gario300@users.noreply.github.com>
2019-09-24 00:35:51 -0500

    Update NotificationController.js

--- app/Controllers/Http/NotificationController.js
+++ app/Controllers/Http/NotificationController.js
@@ -89,7 +89,7 @@ async shownotificationreader ({auth , response}){
             .where('receptor_id', user.id)
             .orWhere('receptor_name',user.username)
             .whereNot('user_id', user.id)
-            .where('is_readed', false)
+            .whereNot('is_readed', false)
             .count('* as total')
 
          const total = noti[0].total   
