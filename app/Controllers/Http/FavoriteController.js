1b6c9636b56f0d7b857923a5d9a436ccbf3c29f6
gario300 <41522264+gario300@users.noreply.github.com>
2019-10-02 12:17:55 -0500

    Update FavoriteController.js

--- app/Controllers/Http/FavoriteController.js
+++ app/Controllers/Http/FavoriteController.js
@@ -6,15 +6,24 @@ class FavoriteController {
     
     async favorite ({ request, auth, response }) {
         // get currently authenticated user
-        const user = auth.current.user
-    
         const PostId = await request.input('post_id')
         const post = await Post.findByOrFail('id', request.input('post_id'))
+        
+        const user = auth.current.user
+        const user2 = await User.findByOrFail('id', post.user_id)
     
         const favorite = await Favorite.findOrCreate(
             { user_id: user.id, post_id: PostId },
             { user_id: user.id, post_id: PostId }
         )
+
+        if(user.puntos >= 0){
+            user.puntos = user.puntos - 10
+            await user.save()
+
+            user2.puntos = user2.puntos + 10
+            await user2.save()
+        }
         
         await post.contadorf ++
         await post.save()
