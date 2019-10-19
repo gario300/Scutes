5638e3198575bfb1f23a3271d55fa9e0b30ff3ce
gario300 <41522264+gario300@users.noreply.github.com>
2019-10-19 09:27:57 -0500

    Update socket.js

--- start/socket.js
+++ start/socket.js
@@ -2,5 +2,5 @@ const Server = use('Server')
 const io = use('socket.io')(Server.getInstance())
 
 io.on('connection', function (socket) {
-  console.log(socket.id)
+  console.log('hello world socket')
 })
