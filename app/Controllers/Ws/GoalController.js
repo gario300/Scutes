81e1eee83849c4a3dfb50dd1c0e4cd1a956b81f2
gario300 <41522264+gario300@users.noreply.github.com>
2019-11-06 23:30:32 -0600

    Update GoalController.js

--- app/Controllers/Ws/GoalController.js
+++ app/Controllers/Ws/GoalController.js
@@ -1,10 +1,11 @@
 'use strict'
 
 class GoalController {
-  constructor ({ socket, request }) {
+  constructor (params) {
+    const { socket, request } = params
     this.socket = socket
     this.request = request
-    
+
     console.log('A new subscription for room topic', socket.topic)
   }
 }
